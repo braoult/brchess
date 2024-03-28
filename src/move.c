92d6909546c90efe18408d914b3a63805284558e
Bruno Raoult <braoult@gmail.com>
2024-03-28 10:37:52 +0100

    update (C) notice

--- src/move.c
+++ src/move.c
@@ -1,6 +1,6 @@
 /* move.c - move management.
  *
- * Copyright (C) 2021 Bruno Raoult ("br")
+ * Copyright (C) 2021-2024 Bruno Raoult ("br")
  * Licensed under the GNU General Public License v3.0 or later.
  * Some rights reserved. See COPYING.
  *
