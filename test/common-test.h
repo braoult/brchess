a8f7bd12b8231d505197ea5df4ccb794c002a976
Bruno Raoult <braoult@gmail.com>
2024-07-03 21:13:15 +0200

    add tricky FEN from talkchess

--- test/common-test.h
+++ test/common-test.h
@@ -210,6 +210,10 @@ struct fentest {
      * Some FEN found on talkchess forum *
      *************************************/
     { __LINE__, FEN | MOVEGEN | MOVEDO | PERFT,
+      "https://www.talkchess.com/forum3/viewtopic.php?f=7&t=71379",
+      "8/6kR/8/8/8/bq6/1rqqqqqq/K1nqnbrq b - - 0 1"
+    },
+    { __LINE__, FEN | MOVEGEN | MOVEDO | PERFT,
       "from https://www.talkchess.com/forum/viewtopic.php?t=42463",
       "rnbqkb1r/pp1p1ppp/2p5/4P3/2B5/8/PPP1NnPP/RNBQK2R w KQkq - 0 6"
     },
